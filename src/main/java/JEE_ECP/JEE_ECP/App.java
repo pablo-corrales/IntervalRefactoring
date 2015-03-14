8a6ccb767afd604e4724f3edf4c4e740a6e4f12b
pablo-corrales <charliequito@hotmail.com>
2015-03-14 10:00:42 +0100

    Primer commit

---
+++ src/main/java/JEE_ECP/JEE_ECP/App.java
@@ -0,0 +1,13 @@
+package JEE_ECP.JEE_ECP;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
