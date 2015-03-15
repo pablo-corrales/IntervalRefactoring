4d7c129bfcf9856530b1ce703619728c6d2d9c70
pablo-corrales <charliequiro>
2015-03-16 00:05:03 +0100

    Se crea clase VotoDAOJPA

--- JEE-ECP/src/main/java/persistence/models/daos/jpa/VotoDAOJPA.java
+++ JEE-ECP/src/main/java/persistence/models/daos/jpa/VotoDAOJPA.java
@@ -1,46 +1,12 @@
 package persistence.models.daos.jpa;
 
-import java.util.List;
-
-import persistence.jpa.Voto;
 import persistence.models.daos.VotoDAO;
+import persistence.models.entities.Voto;
 
-public class VotoDAOJPA implements VotoDAO {
+public class VotoDAOJPA extends GenericDAOJPA<Voto,Integer>implements VotoDAO {
 
-	@Override
-	public void create(Voto entity) {
-		// TODO Auto-generated method stub
+    public VotoDAOJPA() {
+        super(Voto.class);
+    }
 
-	}
-
-	@Override
-	public Voto read(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void update(Voto entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void delete(Voto entity) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void deleteByID(Integer id) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public List<Voto> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-}
+ }
