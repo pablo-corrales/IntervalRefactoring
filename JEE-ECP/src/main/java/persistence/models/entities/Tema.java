002a54f87ba04619857c8ec2d6526d54bb142c10
pablo-corrales <charliequiro>
2015-03-15 22:17:10 +0100

    Se crea clases Tema y Voto como entities, para usarlas en persistencia JDBC

---
+++ JEE-ECP/src/main/java/persistence/models/entities/Tema.java
@@ -0,0 +1,44 @@
+package persistence.models.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+
+@Entity
+public class Tema {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    
+    private String nombre;
+    private String pregunta;
+    
+	public Tema() {
+	}
+
+	public Tema(String nombre, String pregunta) {
+		this.nombre = nombre;
+		this.pregunta = pregunta;
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getPregunta() {
+		return pregunta;
+	}
+	
+	public void setPregunta(String pregunta) {
+		this.pregunta = pregunta;
+	}
+	
+}
