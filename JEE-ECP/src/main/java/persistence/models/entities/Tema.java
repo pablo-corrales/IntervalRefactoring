19c63ee256a6a11450961d29a3ff17309aa2f874
pablo-corrales <charliequiro>
2015-03-16 01:02:02 +0100

    se refactoriza Tema

--- JEE-ECP/src/main/java/persistence/models/entities/Tema.java
+++ JEE-ECP/src/main/java/persistence/models/entities/Tema.java
@@ -44,6 +44,10 @@ public class Tema {
 	public Integer getId() {
 		return id;
 	}
+		
+	public void setId(Integer id) {
+        this.id = id;
+    }
 	
 	public String getNombre() {
 		return nombre;
