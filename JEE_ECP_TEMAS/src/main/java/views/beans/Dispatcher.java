4bd9d71e4ea61ddca9e65515482b11e864c0d6d7
pablo-corrales <charliequiro>
2015-03-29 17:35:31 +0200

    se refactoriza dispatcher para corregir error de factoria de controlador

--- JEE_ECP_TEMAS/src/main/java/views/beans/Dispatcher.java
+++ JEE_ECP_TEMAS/src/main/java/views/beans/Dispatcher.java
@@ -2,6 +2,8 @@ package views.beans;
 
 
 
+import java.io.IOException;
+
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,53 +11,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.logging.log4j.LogManager;
-
-import java.io.IOException;
-
+import persistence.models.entities.Tema;
 import controllers.ControllerFactory;
 import controllers.ejb.ControllerFactoryEJB;
-import persistence.models.entities.Tema;
 
 
 @WebServlet("/jsp/*")
 public class Dispatcher extends HttpServlet {
 	
     private static final long serialVersionUID = 1L;
-    private ControllerFactory controllerFactory;
+    private ControllerFactory controllerFactory = new ControllerFactoryEJB();;
 
     private static String PATH_ROOT_VIEW = "/views/jsp/";
+   
 
-    public void init( ServletConfig conf ) throws ServletException {
-		controllerFactory = new ControllerFactoryEJB();
-	}
-    
-    @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+    	
+    	System.out.println(">>>");
 
-        String action = request.getPathInfo().substring(1);
-        LogManager.getLogger().debug("action::"+action);
+		String action = request.getPathInfo().substring(1);
+
         String view;
-        request.setCharacterEncoding("UTF-8");
+        
         switch (action) {
         case "addTema":
-        	AddTemaBean addTemaBean = new AddTemaBean();
-        	addTemaBean.setTema(new Tema());
-        	addTemaBean.setControllerFactory(this.controllerFactory);
-            request.setAttribute("addTemaBean", addTemaBean);
-            view = "addTema";
+        	AddTemaBean addTemaBean = new AddTemaBean();      	
+            request.setAttribute(action, addTemaBean);
+            view = action;
             break;
         default:
             view = "home";
         }
-        
-        LogManager.getLogger().debug("view::"+view);
-        //this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp")
-        this.getServletContext().getRequestDispatcher("/views/jsp/home.jsp").forward(request, response);
-
+                            
+         this.getServletContext().getRequestDispatcher(PATH_ROOT_VIEW + view + ".jsp").forward(request, response);
+       
+      
     }
 
+    
+    
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -65,12 +60,11 @@ public class Dispatcher extends HttpServlet {
         switch (action) {
         case "addTema":
             tema = new Tema(request.getParameter("nombre"), request.getParameter("pregunta"));
-            tema.setId(Integer.parseInt(request.getParameter("id")));
             AddTemaBean addTemaBean = new AddTemaBean(tema);      
             addTemaBean.setControllerFactory(controllerFactory);
             addTemaBean.process();
             request.setAttribute("addTemaBean", addTemaBean);
-            view = action;
+            view = "home";
             break;
         
         }
