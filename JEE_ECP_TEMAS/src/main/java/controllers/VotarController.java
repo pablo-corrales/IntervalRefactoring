3224334cf9e94aeb16a71a2edaa2c44a8a25d83e
pablo-corrales <charliequiro>
2015-03-29 21:33:21 +0200

    se crea la interfaz para listarTemas

--- JEE_ECP_TEMAS/src/main/java/controllers/VotarController.java
+++ JEE_ECP_TEMAS/src/main/java/controllers/VotarController.java
@@ -1,9 +1,13 @@
 package controllers;
 
+import java.util.List;
+
 import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
 
 public interface VotarController {
+	
+	List<Tema> listarTemas();
 	
 	void votar(Tema tema, Voto voto);
 
