a72f634d95a97d6aa64e29ebe54af7e229069a64
pablo-corrales <charliequiro>
2015-03-30 23:39:44 +0200

    se refactoriza

--- JEE_ECP_TEMAS/src/main/java/controllers/ControllerFactory.java
+++ JEE_ECP_TEMAS/src/main/java/controllers/ControllerFactory.java
@@ -3,11 +3,8 @@ package controllers;
 public abstract class ControllerFactory {
 
 	public abstract AddTemaController getAddTemaController();
-
-	public abstract EliminarTemaController getEliminarTemaController();
-
-	public abstract VerVotacionesController getVerVotacionesController();
-
 	public abstract VotarController getVotarController();
+	public abstract VerVotacionesController getVerVotacionesController();
+	public abstract EliminarTemaController getEliminarTemaController();
 
 }
