57d22f27161a600a0a8861dd89dd63d1c4bf97b7
pablo-corrales <charliequiro>
2015-03-30 23:40:29 +0200

    se refactoriza votarcontroller

--- JEE_ECP_TEMAS/src/main/java/controllers/VotarController.java
+++ JEE_ECP_TEMAS/src/main/java/controllers/VotarController.java
@@ -3,12 +3,11 @@ package controllers;
 import java.util.List;
 
 import persistence.models.entities.Tema;
-import persistence.models.utils.NivelEstudios;
 
 public interface VotarController {
 	
 	List<Tema> listarTemas();
 	
-	void votar(int idTema, NivelEstudios estudios, int puntuacion, String ip);
+	void votar(Integer idTema, Integer estudios, Integer puntuacion, String ip);
 
 }
