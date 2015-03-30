e72cff71259c09281c8f65ba8482f5b66188c87b
pablo-corrales <charliequiro>
2015-03-30 22:02:19 +0200

    se corrige interfaz de Votar

--- JEE_ECP_TEMAS/src/main/java/controllers/ejb/VotarControllerEJB.java
+++ JEE_ECP_TEMAS/src/main/java/controllers/ejb/VotarControllerEJB.java
@@ -5,20 +5,23 @@ import java.util.List;
 import controllers.VotarController;
 import persistence.models.daos.DAOFactory;
 import persistence.models.daos.TemaDAO;
+import persistence.models.daos.VotoDAO;
 import persistence.models.daos.jpa.DAOJPAFactory;
 import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
+import persistence.models.utils.NivelEstudios;
 
 public class VotarControllerEJB implements VotarController{
 	
 	
-	@Override
-	public void votar(Tema tema, Voto voto) {
+	public void votar(int idTema, NivelEstudios estudios, int puntuacion, String ip){
 		DAOFactory.setFactory(new DAOJPAFactory());
         TemaDAO temaDAO = DAOFactory.getFactory().getTemaDAO();
-        temaDAO.read(tema.getId()).addVoto(voto);
-        temaDAO.update(temaDAO.read(tema.getId()));
-	 }
+		Tema tema = temaDAO.read(idTema);
+		VotoDAO votoDAO = DAOFactory.getFactory().getVotoDAO();
+		Voto voto = new Voto(puntuacion, estudios, ip, tema);
+		votoDAO.create(voto);
+	}
 	
 	
 	@Override
