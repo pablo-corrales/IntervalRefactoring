3e3d505cdf551f861c29ae781412b5f0a9e4baba
pablo-corrales <charliequiro>
2015-03-30 23:30:06 +0200

    se refactoriza Tema

--- JEE_ECP_TEMAS/src/main/java/persistence/models/entities/Tema.java
+++ JEE_ECP_TEMAS/src/main/java/persistence/models/entities/Tema.java
@@ -1,14 +1,8 @@
 package persistence.models.entities;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
