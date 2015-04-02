fac8bfaff5ea898711bd0769b0c6552fdc3be429
pablo-corrales <charliequiro>
2015-04-02 12:00:49 +0200

    se refactoriza VotarController con la correción de errores para presentar en el Bean de Voto

--- JEE_ECP_TEMAS/src/main/java/controllers/ejb/VotarControllerEJB.java
+++ JEE_ECP_TEMAS/src/main/java/controllers/ejb/VotarControllerEJB.java
@@ -2,11 +2,14 @@ package controllers.ejb;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+
 import controllers.VotarController;
 import persistence.models.daos.DAOFactory;
 import persistence.models.daos.TemaDAO;
 import persistence.models.daos.VotoDAO;
 import persistence.models.daos.jpa.DAOJPAFactory;
+import persistence.models.daos.jpa.GenericDAOJPA;
 import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
 
@@ -14,6 +17,7 @@ public class VotarControllerEJB implements VotarController{
 	
 	
 	public void votar(Integer idTema, Integer estudios, Integer puntuacion, String ip){
+		 LogManager.getLogger(GenericDAOJPA.class).debug(">>>votar ");
 		DAOFactory.setFactory(new DAOJPAFactory());
         TemaDAO temaDAO = DAOFactory.getFactory().getTemaDAO();
 		Tema tema = temaDAO.read(idTema);
