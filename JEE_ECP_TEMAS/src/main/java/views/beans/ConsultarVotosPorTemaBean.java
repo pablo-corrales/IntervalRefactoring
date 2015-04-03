d183a4b2c758634b08b822df653cfe6e6f776a88
pablo-corrales <charliequiro>
2015-04-03 14:01:47 +0200

    se refactoriza dispatcher para que procesar map de votos y temas

--- JEE_ECP_TEMAS/src/main/java/views/beans/ConsultarVotosPorTemaBean.java
+++ JEE_ECP_TEMAS/src/main/java/views/beans/ConsultarVotosPorTemaBean.java
@@ -1,10 +1,13 @@
 package views.beans;
 
 import java.io.Serializable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.servlet.http.HttpServletRequest;
 
 import persistence.models.entities.Tema;
 import persistence.models.entities.Voto;
@@ -17,9 +20,17 @@ public class ConsultarVotosPorTemaBean extends ViewBean implements Serializable{
 	private static final long serialVersionUID = 1L;
 	private List<Tema> temas;
 	private List<Voto> votos;
-	private static final Integer NUMERO_MAXIMO_TEMAS = 5;
-	Integer[] DatosVotos = new Integer[NUMERO_MAXIMO_TEMAS];
+	private Map<String, Integer> votoTema = new HashMap<String, Integer>();
 	
+	
+	public Map<String, Integer> getVotoTema() {
+		return votoTema;
+	}
+
+	public void setVotoTema(Map<String, Integer> votoTema) {
+		this.votoTema = votoTema;
+	}
+
 	public List<Voto> getVotos() {
 		return votos;
 	}
@@ -31,23 +42,29 @@ public class ConsultarVotosPorTemaBean extends ViewBean implements Serializable{
 	public List<Tema> getTemas() {
 		return temas;
 	}
+	
 
 	public void setTemas(List<Tema> temas) {
 		this.temas = temas;
 	}
+	
+	
 
-	public void upate(){
+	public Map<String, Integer> update(HttpServletRequest request){
 		ConsultarVotosPorTemaController consultarVotosPorTemaController = this.getControllerFactory().getConsultarVotosPorTemaController();
 		this.temas = consultarVotosPorTemaController.listarTemas();	
-		
-		Integer contadorVotos = 0;
-		for(Tema tema: temas){			
-			this.votos = consultarVotosPorTemaController.ConsultarVotosPorTema(tema.getId());
-			DatosVotos[contadorVotos] = votos.size();
-			contadorVotos++;
+				
+		for(Tema tema: temas){		
+			
+			this.votos = consultarVotosPorTemaController.ConsultarVotosPorTema(tema);
+			
+			votoTema.put(tema.getNombre(), votos.size());
+			
 		}
 		
+		return votoTema;    
 		
+
 	}
 
 }
