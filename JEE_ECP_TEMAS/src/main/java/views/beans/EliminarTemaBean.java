ef874e64ed9709de32e34f0cd418014d563fe308
pablo-corrales <charliequiro>
2015-04-03 03:23:13 +0200

    se implementa bean para eleiminacion de tema

--- JEE_ECP_TEMAS/src/main/java/views/beans/EliminarTemaBean.java
+++ JEE_ECP_TEMAS/src/main/java/views/beans/EliminarTemaBean.java
@@ -1,100 +1,58 @@
 package views.beans;
 
+import java.io.Serializable;
 import java.util.List;
 
 import javax.annotation.ManagedBean;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ViewScoped;
 
+import org.apache.logging.log4j.LogManager;
+
 import controllers.EliminarTemaController;
+import persistence.models.daos.jpa.GenericDAOJPA;
 import persistence.models.entities.Tema;
 
 @ManagedBean
 @ViewScoped
-public class EliminarTemaBean extends ViewBean {
+public class EliminarTemaBean extends ViewBean  implements Serializable{
 
-	private static final long serialVersionUID = 1L;
-	private static String identificadorAutorizado = "666";
-	private EliminarTemaController eliminarTemaController = this.getControllerFactory().getEliminarTemaController();
+	private static final long serialVersionUID = 1L;	
+	private EliminarTemaController eliminarTemaController = this.getControllerFactory().getEliminarTemaController();	
 	private List<Tema> temas;
-	private int idTema;
-	private String mensajeBorrado;
-	private String identificador;
-	private boolean temaDesabilitado;
-	private boolean borradoDesabilitado;
+	private Integer idTema;
 	
 	public EliminarTemaBean() {
 		super();
+		
 	}
 		
-	public boolean isBorradoDesabilitado() {
-		return borradoDesabilitado;
-	}
-
-	public void setBorradoDesabilitado(boolean borradoDesabilitado) {
-		this.borradoDesabilitado = borradoDesabilitado;
-	}
-
-	
-	public boolean isTemaDesabilitado() {
-		return temaDesabilitado;
-	}
-
-
-	public void setTemaDesabilitado(boolean temaDesabilitado) {
-		this.temaDesabilitado = temaDesabilitado;
-	}
-	public String getMensajeBorrado() {
-		return mensajeBorrado;
-	}
-
-
-	public void setMensajeBorrado(String mensajeBorrado) {
-		this.mensajeBorrado = mensajeBorrado;
-	}
-
-
-	public String getIdentificador() {
-		return identificador;
-	}
-
-	public void setIdentificador(String identificador) {
-		this.identificador = identificador;
-		this.mensajeBorrado="";
-		this.temaDesabilitado = false;
-		this.borradoDesabilitado = false;
-		
-		if (!identificadorAutorizado.equals(identificador)){
-			this.mensajeBorrado = "No está autorizado a esta operación";
-			this.temaDesabilitado = true;
-			this.borradoDesabilitado = true;
-		}
-	}
-
 	public List<Tema> getTemas() {
 		return temas;
 	}
-	
 	
 	public int getIdTema() {
 		return idTema;
 	}
 
-	public void setIdTema(int idTema) {
+	public void setIdTema(Integer idTema) {
 		this.idTema = idTema;
 		
 	}
-
 	
 	@PostConstruct
     public void update() {
+		LogManager.getLogger(GenericDAOJPA.class).debug(">>>update eliminarTemaBean ");
 		this.temas = eliminarTemaController.listarTemas();
-        this.temas.add(0, new Tema(-1, "Elige", ""));
-        this.idTema = -1;
+        this.temas.add(0, new Tema(0, "Temas...", ""));
+        this.idTema = 0;
     }
 	
 	public String process(){
 		eliminarTemaController.eliminarTema(idTema);
 		return "home";
 	}
+	
+	
+	
 }
--- JEE_ECP_TEMAS/src/main/webapp/views/jsp/eliminarTema.jsp
+++ JEE_ECP_TEMAS/src/main/webapp/views/jsp/eliminarTema.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -9,16 +10,6 @@
 <body>
 	<c:set var="rView" scope="request" value="${eliminarTema}"/>
 	<h3>Eliminar Tema</h3>
-	
-	<form action="/JEE_ECP/jsp/eliminarTema" method="post">
-		<p>Código de autorización para borrado:
-			<input type="text" name = "identificador"/>
-		</p>
-		<p>${rView.mensajeBorrado}</p>
-		<input type="submit" value="Verificar" />
-	</form>
-	
-	
 	
 	<form action="/JEE_ECP_TEMAS/jsp/eliminarTema" method="post">
 		<p>Tema:
@@ -30,6 +21,6 @@
 		</p>
 		<input type="submit" value="Borrar" />
 	</form>
-	<a href="/JEE_ECP/jsp/home">Volver a Home</a>
+	<a href="/JEE_ECP_TEMAS/jsp/home">Volver a Home</a>
 </body>
 </html>
