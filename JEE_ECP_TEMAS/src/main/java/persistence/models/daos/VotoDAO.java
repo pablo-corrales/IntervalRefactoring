af94641e7d3aa8e8d4d78f7d1680c7598b4ebea0
pablo-corrales <charliequiro>
2015-04-03 17:36:18 +0200

    se implementa votodao

--- JEE_ECP_TEMAS/src/main/java/persistence/models/daos/VotoDAO.java
+++ JEE_ECP_TEMAS/src/main/java/persistence/models/daos/VotoDAO.java
@@ -10,5 +10,6 @@ public interface VotoDAO extends GenericDAO<Voto, Integer>{
 	
 	List<Voto> findVotoByTema(Tema tema);
 	void deleteVotoByTemaID(Tema tema);
+	List<Voto> findVotoByNivelEstudios(Integer nivelEstudios);
 
 }
