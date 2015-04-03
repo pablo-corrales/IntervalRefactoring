ed64cce83838fd04002f7eff854fe7a38d078a57
pablo-corrales <charliequiro>
2015-04-03 03:27:02 +0200

    se limpia codigo inservible

--- JEE_ECP_TEMAS/src/main/java/persistence/models/entities/Tema.java
+++ JEE_ECP_TEMAS/src/main/java/persistence/models/entities/Tema.java
@@ -4,7 +4,6 @@ import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.GeneratedValue;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
