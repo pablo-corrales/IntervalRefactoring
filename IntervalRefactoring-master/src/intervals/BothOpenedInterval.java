44d7f9b907d39669fea26cfc70dd85793053eecc
pablo-corrales <charliequiro>
2015-04-09 22:54:55 +0200

    commit de vericacion codigo inicial

---
+++ IntervalRefactoring-master/src/intervals/BothOpenedInterval.java
@@ -0,0 +1,14 @@
+package intervals;
+
+public class BothOpenedInterval extends Interval{
+	
+	
+	public BothOpenedInterval(double minimum, double maximum, Opening opening) {
+		super(minimum, maximum, opening);
+	
+	}
+	
+	
+	
+
+}
