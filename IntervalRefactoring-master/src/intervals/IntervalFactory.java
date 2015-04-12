7fca92a4c2ced78c3b0ec2933910e2251ea1e08b
pablo-corrales <charliequiro>
2015-04-12 22:22:26 +0200

    Se refactoriza IntervalFactory para utilizar las clases hijas de Punto. FromPoint  corresponde a  valores > x; UntilPoint corresponde a valores < x; ExactPoint corresponde a valores = x;

--- IntervalRefactoring-master/src/intervals/IntervalFactory.java
+++ IntervalRefactoring-master/src/intervals/IntervalFactory.java
@@ -5,27 +5,27 @@ public class IntervalFactory {
 
 	public static Interval getInterval(double minimum, double maximum, Opening opening) {
 				switch(opening){
-				case BOTH_OPENED: return getBothOpenedInterval(new Point(minimum), new Point(maximum));
-				case LEFT_OPENED: return getLeftOpenedInterval(new Point(minimum), new Point(maximum));
-				case RIGHT_OPENED: return getRigthOpenedInterval(new Point(minimum), new Point(maximum));
-				case UNOPENED: return getUnOpenedInterval(new Point(minimum), new Point(maximum));
+				case BOTH_OPENED: return getBothOpenedInterval(new FromPoint(minimum), new UntilPoint(maximum));
+				case LEFT_OPENED: return getLeftOpenedInterval(new FromPoint(minimum), new ExactPoint(maximum));
+				case RIGHT_OPENED: return getRigthOpenedInterval(new ExactPoint(minimum), new UntilPoint(maximum));
+				case UNOPENED: return getUnOpenedInterval(new ExactPoint(minimum), new ExactPoint(maximum));
 				default: return null;
 			}
 	}
 	
-	public static Interval getBothOpenedInterval(Point minimum, Point maximum) {
+	public static Interval getBothOpenedInterval(FromPoint minimum, UntilPoint maximum) {
 				return new BothOpenedInterval(minimum, maximum);
 	}
 	
-	public static Interval getLeftOpenedInterval(Point minimum, Point maximum) {
+	public static Interval getLeftOpenedInterval(FromPoint minimum, ExactPoint maximum) {
 		return new LeftOpenedInterval(minimum, maximum);
 	}
 	
-	public static Interval getRigthOpenedInterval(Point minimum, Point maximum) {
+	public static Interval getRigthOpenedInterval(ExactPoint minimum, UntilPoint maximum) {
 		return new RightOpenedInterval(minimum, maximum);
 	}
 	
-	public static Interval getUnOpenedInterval(Point minimum, Point maximum) {
+	public static Interval getUnOpenedInterval(ExactPoint minimum, ExactPoint maximum) {
 		return new UnOpenedInterval(minimum, maximum);
 	}
 }
