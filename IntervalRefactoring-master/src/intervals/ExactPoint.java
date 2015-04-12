f96cfc5caec12f3baf18cd70545ee42b33711955
pablo-corrales <charliequiro>
2015-04-12 22:58:47 +0200

    Se implmenta métos en ExactPoint para intervalo cerrados

--- IntervalRefactoring-master/src/intervals/ExactPoint.java
+++ IntervalRefactoring-master/src/intervals/ExactPoint.java
@@ -5,5 +5,15 @@ public class ExactPoint extends Point{
 	public ExactPoint(double value) {
 		super(value);
 	};
+	
+	@Override
+	public boolean included(FromPoint fromPoint) {
+		return fromPoint.getValue() <= this.getValue();
+	}
+	
+	@Override
+	public boolean included(UntilPoint untilPoint) {
+			return this.getValue() <= untilPoint.getValue();
+	}
 
 }
