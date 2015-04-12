2dca6b10c5ce092a6fb9277491d600399d9d7950
pablo-corrales <charliequiro>
2015-04-12 22:43:56 +0200

    Se refactoriza la clase Point para implementar los intervalos abiertos en clases hijas

--- IntervalRefactoring-master/src/intervals/Point.java
+++ IntervalRefactoring-master/src/intervals/Point.java
@@ -1,6 +1,6 @@
 package intervals;
 
-public class Point {
+public abstract class Point {
 	
 	protected double value;
 	
@@ -16,6 +16,18 @@ public class Point {
 		this.value = value;
 	}
 	
+	public boolean includes(Point p){
+				return true;
+	}
+			
+	public abstract boolean included(FromPoint fromPoint);	
+	public abstract boolean included(UntilPoint untilPoint);
+			
+	@Override
+	public boolean equals(Object object) {
+		 		Point point = (Point) object;
+		 		return this.getValue() == point.getValue();
+	}
 	
 
 }
