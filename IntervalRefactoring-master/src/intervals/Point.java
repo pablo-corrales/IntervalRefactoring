674f3a7093db0b414c8f13c7505890e34349e1a8
pablo-corrales <charliequiro>
2015-04-12 13:22:38 +0200

    se implementa la clase punto como abstracta con la firma de los métodos que serviran para definir los intervalos

--- IntervalRefactoring-master/src/intervals/Point.java
+++ IntervalRefactoring-master/src/intervals/Point.java
@@ -1,5 +1,27 @@
 package intervals;
 
-public class Point {
+public abstract class Point {
+	
+	protected double value;
+	
+	public Point(double value) {
+		this.value = value;
+	}
+	
+	public abstract boolean esMenorQue(double value);
+	
+	public abstract boolean esMayorQue(double value);
+	
+	public abstract boolean esMenorIgualQue(double value);
+	
+	public abstract boolean esMayorIgualQue(double value);
+
+	public double getValue() {
+		return value;
+	}
+
+	public void setValue(double value) {
+		this.value = value;
+	}
 
 }
