6fc75fdfce03659eb26fc8ae98cd1d790d5bfc51
pablo-corrales <charliequiro>
2015-04-12 23:05:34 +0200

    Se crean métodos en la clase UntilPoint para inclusion de intervalos abiertos a derecha

--- IntervalRefactoring-master/src/intervals/UntilPoint.java
+++ IntervalRefactoring-master/src/intervals/UntilPoint.java
@@ -5,4 +5,19 @@ public class UntilPoint extends Point{
 	public UntilPoint(double value) {
 		super(value);
 	};
+	
+	
+	public boolean includes(Point point){
+			return point.included(this);
+	}
+	
+	@Override
+	public boolean included(FromPoint fromPoint) {
+			return fromPoint.getValue() < this.getValue();
+	}
+	
+	@Override
+	public boolean included(UntilPoint untilPoint) {
+			return this.getValue() <= untilPoint.getValue();
+	}
 }
